5acb5fed25ba9425cc52b11eb71cd79b8b88acbd
Hasier Pastor <hasier.pastor@gmail.com>
2019-02-26 12:42:59 -0800

    added some module pattern examples

--- modulePattern.js
+++ modulePattern.js
@@ -6,3 +6,58 @@
 //Used to separate logic and concerns.
 
 //Definition from Learning JavaScript Design Patterns by Addy Osmani - Published by O'Reilly Media, Inc., 2012
+
+let uniqueId = (function() {
+  //private variable
+  let id = 0;
+  //could add a private method here
+
+  //return an object exposed to the public
+  return {
+    //public method to increment id
+    incrementId: function() {
+      return ++id;
+    },
+    //public method to reset id
+    resetId: function() {
+      console.log(`id val before resetting: ${id}`);
+      id = 0;
+    }
+  };
+})();
+
+console.log(uniqueId.incrementId());
+console.log(uniqueId.incrementId());
+console.log(uniqueId.resetId());
+
+let boardGameCollection = (function() {
+  let boardGames = [];
+
+  return {
+    addBoardGame: function(boardGame) {
+      boardGames.push(boardGame);
+      return 'Board game added';
+    },
+    numOfBoardGames: function() {
+      return boardGames.length;
+    }
+  };
+})();
+
+console.log(
+  boardGameCollection.addBoardGame({
+    name: 'Near And Far',
+    designer: 'Ryan Laukat'
+  })
+);
+
+console.log(
+  boardGameCollection.addBoardGame({
+    name: 'Brass Birmingham',
+    designer: 'Martin Wallace'
+  })
+);
+
+console.log(boardGameCollection.numOfBoardGames()); //2
+
+console.log(boardGames); //should return reference error cause it does not exist in our returned public object
