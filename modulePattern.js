f7ac8dceda39dc3b9f0b4ba566cf470d91ca6672
Hasier Pastor <hasier.pastor@gmail.com>
2019-02-26 12:24:17 -0800

    added definition for module pattern

---
+++ modulePattern.js
@@ -0,0 +1,8 @@
+//The Module pattern encapsulates “privacy” state and organization using closures.
+//It provides a way of wrapping a mix of public and private methods and variables, protecting pieces from leaking into the global scope and accidentally colliding with another developer’s interface.
+//With this pattern, only a public API is returned, keeping everything else within the closure private.
+//Module pattern imitates the functionality of a class, including private/public methods, and variables inside an object, shielding them from the global scope.
+//Very similar to an immediately-invoked functional expression, except that an object is returned rather than a function.
+//Used to separate logic and concerns.
+
+//Definition from Learning JavaScript Design Patterns by Addy Osmani - Published by O'Reilly Media, Inc., 2012
